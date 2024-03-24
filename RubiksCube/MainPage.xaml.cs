6fdc45b2348ed873263b16d86b708dd0012ba754
Geert Geerits <ggeerits@hotmail.com>
2024-03-24 16:29:56 +0100

    Update to version 2.0.19

--- RubiksCube/MainPage.xaml.cs
+++ RubiksCube/MainPage.xaml.cs
@@ -1,8 +1,8 @@
 ﻿// Program .....: RubiksCube.sln
 // Author ......: Geert Geerits - E-mail: geertgeerits@gmail.com
 // Copyright ...: (C) 1981-2024
-// Version .....: 2.0.18
-// Date ........: 2024-03-23 (YYYY-MM-DD)
+// Version .....: 2.0.19
+// Date ........: 2024-03-24 (YYYY-MM-DD)
 // Language ....: Microsoft Visual Studio 2022: .NET MAUI 8 - C# 12.0
 // Description .: Solving the Rubik's Cube
 // Note ........: This program is based on the program 'SolCube' I wrote in 1981 in MS Basic-80 for a Commodore PET 2001
