0a9216546e2070ce70a4c92e286a70c39caba00f
Geert Geerits <ggeerits@hotmail.com>
2024-12-22 10:42:28 +0100

    Update

--- RubiksCube/MauiProgram.cs
+++ RubiksCube/MauiProgram.cs
@@ -14,7 +14,8 @@ namespace RubiksCube
     			{
     				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
     				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-    			})
+                    fonts.AddFont("PublicSans-Bold.ttf", "PublicSansBold");
+                })
 
                 .ConfigureLifecycleEvents(events =>
                 {
