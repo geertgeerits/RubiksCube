85ac26acad10af2ba814aa9350fd09603a537091
Geert Geerits <ggeerits@hotmail.com>
2025-01-03 12:55:46 +0100

    Update

--- RubiksCube/ClassSolveCubeCFOP_FTL.cs
+++ RubiksCube/ClassSolveCubeCFOP_FTL.cs
@@ -84,18 +84,172 @@ namespace RubiksCube
                     await MakeTurnAsync("y");
                 }
 
+                // When two adjacent faces (front and right) have been solved, turn the cube
+                if (aPieces[49] == aPieces[46] && aPieces[49] == aPieces[47] && aPieces[49] == aPieces[50])
+                {
+                    if (aPieces[4] == aPieces[5] && aPieces[4] == aPieces[7] && aPieces[4] == aPieces[8])
+                    {
+                        if (aPieces[13] == aPieces[12] && aPieces[13] == aPieces[15] && aPieces[13] == aPieces[16])
+                        {
+                            await MakeTurnAsync("y");
+                        }
+                    }
+                }
+
+                // Bring the corner piece at the top to the right position on the top layer
+                if (aPieces[49] == aPieces[11] || aPieces[49] == aPieces[18] || aPieces[49] == aPieces[38])
+                {
+                    if (aPieces[4] == aPieces[11] || aPieces[4] == aPieces[18] || aPieces[4] == aPieces[38])
+                    {
+                        if (aPieces[13] == aPieces[11] || aPieces[13] == aPieces[18] || aPieces[13] == aPieces[38])
+                        {
+                            await MakeTurnAsync("U");
+                        }
+                    }
+                }
+
+                if (aPieces[49] == aPieces[20] || aPieces[49] == aPieces[27] || aPieces[49] == aPieces[36])
+                {
+                    if (aPieces[4] == aPieces[20] || aPieces[4] == aPieces[27] || aPieces[4] == aPieces[36])
+                    {
+                        if (aPieces[13] == aPieces[20] || aPieces[13] == aPieces[27] || aPieces[13] == aPieces[36])
+                        {
+                            await MakeTurnAsync("U2");
+                        }
+                    }
+                }
+
+                if (aPieces[49] == aPieces[0] || aPieces[49] == aPieces[29] || aPieces[49] == aPieces[42])
+                {
+                    if (aPieces[4] == aPieces[0] || aPieces[4] == aPieces[29] || aPieces[4] == aPieces[42])
+                    {
+                        if (aPieces[13] == aPieces[0] || aPieces[13] == aPieces[29] || aPieces[13] == aPieces[42])
+                        {
+                            await MakeTurnAsync("U'");
+                        }
+                    }
+                }
+
                 //--------------------------------------------------------------------------------------------------------------
 
                 // https://www.myrubik.com/descarregues.php?lang=en
-                
-                //if (aPieces[49] == aPieces[44] && aPieces[4] == aPieces[3] && aPieces[4] == aPieces[6] && aPieces[4] == aPieces[7] && aPieces[4] == aPieces[9] && aPieces[4] == aPieces[19])
-                //{
-                //    if (aPieces[13] == aPieces[2] && aPieces[13] == aPieces[14] && aPieces[13] == aPieces[16] && aPieces[13] == aPieces[17] && aPieces[13] == aPieces[37])
-                //    {
-                //        await MakeTurnAsync("U2 F' U' F");
-                //        continue;
-                //    }
-                //}
+
+                // 1.1
+                if (aPieces[49] == aPieces[2] && aPieces[4] == aPieces[3] && aPieces[4] == aPieces[6] && aPieces[4] == aPieces[7] && aPieces[4] == aPieces[41] && aPieces[4] == aPieces[44])
+                {
+                    if (aPieces[13] == aPieces[9] && aPieces[13] == aPieces[10] && aPieces[13] == aPieces[14] && aPieces[13] == aPieces[16] && aPieces[13] == aPieces[17])
+                    {
+                        await MakeTurnAsync("U R U' R'");
+                        continue;
+                    }
+                }
+
+                // 1.2
+                if (aPieces[49] == aPieces[9] && aPieces[4] == aPieces[1] && aPieces[4] == aPieces[2] && aPieces[4] == aPieces[3] && aPieces[4] == aPieces[6] && aPieces[4] == aPieces[7])
+                {
+                    if (aPieces[13] == aPieces[14] && aPieces[13] == aPieces[16] && aPieces[13] == aPieces[17] && aPieces[13] == aPieces[43] && aPieces[13] == aPieces[44])
+                    {
+                        await MakeTurnAsync("U' F' U F");
+                        continue;
+                    }
+                }
+
+                // 1.3
+                if (aPieces[49] == aPieces[2] && aPieces[4] == aPieces[3] && aPieces[4] == aPieces[6] && aPieces[4] == aPieces[7] && aPieces[4] == aPieces[44])
+                {
+                    if (aPieces[13] == aPieces[9] && aPieces[13] == aPieces[14] && aPieces[13] == aPieces[16] && aPieces[13] == aPieces[17] && aPieces[13] == aPieces[39])
+                    {
+                        await MakeTurnAsync("F' U' F");
+                        continue;
+                    }
+                }
+
+                // 1.4
+                if (aPieces[49] == aPieces[9] && aPieces[4] == aPieces[2] && aPieces[4] == aPieces[3] && aPieces[4] == aPieces[6] && aPieces[4] == aPieces[7] && aPieces[4] == aPieces[37])
+                {
+                    if (aPieces[13] == aPieces[14] && aPieces[13] == aPieces[16] && aPieces[13] == aPieces[17] && aPieces[13] == aPieces[44])
+                    {
+                        await MakeTurnAsync("R U R'");
+                        continue;
+                    }
+                }
+
+                // 1.5
+
+                // 1.6
+
+                // 1.7
+
+                // 1.8
+
+                // 1.9
+
+                // 1.10
+
+                // 1.11
+
+                // 1.12
+
+                // 1.13
+
+                // 1.14
+
+                // 1.15
+
+                // 1.16
+
+                // 1.17
+
+                // 1.18
+
+                // 1.19
+
+                // 1.20
+
+                // 1.21
+
+                // 1.22
+
+                // 1.23
+
+                // 1.24
+
+                // 2.1
+
+                // 2.2
+
+                // 2.3
+
+                // 2.4
+
+                // 2.5
+
+                // 2.6
+
+                // 2.7
+
+                // 2.8
+
+                // 2.9
+
+                // 2.10
+
+                // 2.11
+
+                // 2.12
+
+                // 2.13
+
+                // 2.14
+
+                // 2.15
+
+                // 2.16
+
+                // 2.17
+
+                // 2.18
+
 
             }
 
